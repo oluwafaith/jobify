ac7f4b76c8b33702f3e221da5ee69715e82f7838
faith okpala <okpalafaith1@gmail.com>
2022-03-28 15:46:39 +0100

    error page worked on

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing  from "./pages/Landing";
+import{ Landing, Error, Register, Dashboard } from "./pages"
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      
+    <Route path='/' element={<Dashboard />} />
+    <Route path='/register' element={<Register />} />
       <Route path='/landing' element={<Landing />} />
-    
+      <Route path='*' element={<Error />} />
     </Routes>
   </BrowserRouter>
   );
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,6 @@
-import Dashboard from '.Dashboard'
+import Dashboard from './Dashboard'
 import Landing from './Landing'
 import Error from './Error'
 import Register from './Register'
+
+export { Landing, Error, Register, Dashboard }
