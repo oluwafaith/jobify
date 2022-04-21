aef7220c22bf34397c683527dfa4d21e691a2211
faith okpala <okpalafaith1@gmail.com>
2022-04-21 16:07:49 +0100

    basic structure

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
 import express from "express"
 const app = express()
+import dotenv from 'dotenv'
+dotenv.config()
+//middleware
+import notFoundMiddleware from "./not-found.js"
+import errorHandlerMiddleware from "./error-handler.js"
+//db and authenticate user
+import connectDB from "./db/connect.js"
 
 
+//routers
+import authRouter from "./routes/authRoutes.js"
+import jobsRouter from "./routes/jobsRoutes.js"
+
+app.use(express.json())
 app.get('/', (req, res)=>{
+    throw new Error('error')
 res.send('welcome ...')
 })
 
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
 const port = process.env.PORT || 5000;
-app.listen(port, ()=>{
-    console.log(`server is listening on port ${port}`);
-})
+
+const start  = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, ()=>{
+            console.log(`server is listening on port ${port}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
