59efd97920a28643d9b3b4bf511f5eae817c178d
faith okpala <okpalafaith1@gmail.com>
2022-04-22 14:10:05 +0100

    worked pn auth

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 import mongoose   from "mongoose";
 import validator from 'validator'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -23,7 +24,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please provide password'],
         minlength: 6,
-        // select: false,
+        select: false,
       },
       lastName: {
         type: String,
@@ -38,5 +39,17 @@ const UserSchema = new mongoose.Schema({
         default: 'my city',
       },
 })
+UserSchema.pre('save', async function () {
+    
+    // if (!this.isModified('password')) return
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+  })
+
+  UserSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_LIFETIME,
+    })
+  }
 
 export default mongoose.model('User', UserSchema)
