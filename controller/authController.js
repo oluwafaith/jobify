6f849df270e546ceec8065b9adfa38314d981e2e
faith okpala <okpalafaith1@gmail.com>
2022-04-22 13:49:04 +0100

    worked on error handling

--- controller/authController.js
+++ controller/authController.js
@@ -1,8 +1,24 @@
-// const res = require("express/lib/response")
+import User from '../models/User.js'
+import { StatusCodes } from 'http-status-codes'
+import {BadRequestError} from "../errors/index.js"
 
-const register = async(req, res)=>{
-    res.send('register user')
-}
+const register = async (req, res) => {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        throw new BadRequestError('please provide all values')
+      }
+
+      const userAlreadyExists = await User.findOne({ email })
+      if (userAlreadyExists) {
+        throw new BadRequestError('Email already in use')
+      }
+    
+        const user = await User.create({ name, email, password })
+        res.status(StatusCodes.CREATED).json({user})
+
+  }
+
 const login = async(req, res)=>{
     res.send('login user')
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express"
 const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
+import "express-async-errors"
 //middleware
 import notFoundMiddleware from "./not-found.js"
 import errorHandlerMiddleware from "./error-handler.js"
