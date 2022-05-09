2573490fb321018b5b8a17f832057a277bbe6fce
faith okpala <okpalafaith1@gmail.com>
2022-05-09 15:30:57 +0100

    deployment steps

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV !== 'production') {
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // only when ready to deploy
-// app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.static(path.resolve(__dirname, './client/build')))
 
 app.use(express.json())
 app.use(helmet())
